0d85f69db5aa687d47e308417a09db5889ec1768
Vladimir Sorokin <v.sorokin@hey.com>
2024-12-26 14:56:01 +0200

    Fix tests for updated encode func

--- internal/shortener/encode_test.go
+++ internal/shortener/encode_test.go
@@ -3,6 +3,8 @@ package shortener
 import (
 	"fmt"
 	"testing"
+
+	"github.com/go-redis/redismock/v9"
 )
 
 type TestCase struct {
@@ -11,9 +13,15 @@ type TestCase struct {
 }
 
 func TestEncode(t *testing.T) {
+	db, mock := redismock.NewClientMock()
+
+	// Emulated this value already exist in DB,
+	// so need to add one more symbol to key
+	mock.ExpectGet("NcaA").SetVal("value")
+
 	for _, tt := range TestData {
 		t.Run(fmt.Sprintf("Encode %d", tt.number), func(t *testing.T) {
-			result := encode(tt.number)
+			result := encode(tt.number, db)
 			if result != tt.encodedStr {
 				t.Errorf("Encode(%d) = %s; expected %s", tt.number, result, tt.encodedStr)
 			}
@@ -26,8 +34,8 @@ var TestData = []TestCase{
 	{number: 1, encodedStr: "b"},
 	{number: 998, encodedStr: "1u"},
 	{number: 999, encodedStr: "2u"},
-	{number: 15000000, encodedStr: "jkAGb"},
-	{number: 100000000, encodedStr: "tWAkm"},
+	{number: 15000000, encodedStr: "jkAG"},
+	{number: 100000000, encodedStr: "tWAk"},
 	{number: 10474085661195148593, encodedStr: "VJbd"},
 	{number: 10998849939277099907, encodedStr: "PQzM"},
 	{number: 3366573584661143826, encodedStr: "vtHk"},
@@ -38,7 +46,7 @@ var TestData = []TestCase{
 	{number: 11660458361119275188, encodedStr: "pPFE"},
 	{number: 7409133607407091621, encodedStr: "fQh2"},
 	{number: 3277694648817963827, encodedStr: "weVv"},
-	{number: 18176449534139642514, encodedStr: "NcaAb"},
+	{number: 18176449534139642514, encodedStr: "NcaAu"},
 	{number: 4507735593119348904, encodedStr: "3bMz"},
 	{number: 457861098005776618, encodedStr: "Nhy8"},
 	{number: 14796952220313002321, encodedStr: "brVp"},
@@ -48,5 +56,5 @@ var TestData = []TestCase{
 	{number: 7571599580588466967, encodedStr: "26Ab"},
 	{number: 15331931484882232747, encodedStr: "6CUf"},
 	{number: 6867411904295255519, encodedStr: "JUxj"},
-	{number: 18446744073709551615, encodedStr: "sDC7b"},
+	{number: 18446744073709551615, encodedStr: "sDC7"},
 }
