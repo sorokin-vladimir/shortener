1b2039e2a14abfb3b65161a5a8b97ad7dcb54b38
Vladimir Sorokin <v.sorokin@hey.com>
2025-01-17 22:27:34 +0200

    Client: used HTMX; created simple layout and base functional

--- client/src/main.js
+++ client/src/main.js
@@ -1,1 +1,32 @@
-console.log(`test`, );
+document.body.addEventListener("htmx:afterSettle", function(event) {
+  const target = event.target;
+  if (target.id !== "partial-content") {
+    return;
+  }
+
+  const newButton = document.getElementById("copy-short");
+  if (!newButton) {
+    return;
+  }
+
+  newButton.addEventListener("click", copyHandler)
+})
+
+function copyHandler() {
+  if (!navigator.clipboard) {
+    const input = document.getElementById("short-input");
+    input.select();
+    input.setSelectionRange(0, input.value.length);
+
+    try {
+        document.execCommand("copy");
+    } catch (err) {
+        console.error("Failed to copy text: ", err);
+    }
+    input.blur();
+    return;
+  }
+
+  const textToCopy = document.getElementById("short").textContent;
+  navigator.clipboard.writeText(textToCopy)
+}
